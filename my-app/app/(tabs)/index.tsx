5eff8b4c3ce1abd0b39e93b695a567a3ae4af3d8
Yann <yann.lestage@gmail.com>
2025-03-05 14:35:26 +0100

    init projet

---
+++ my-app/app/(tabs)/index.tsx
@@ -0,0 +1,35 @@
+import { StyleSheet } from 'react-native';
+
+import EditScreenInfo from '@/components/EditScreenInfo';
+import { Text, View } from '@/components/Themed';
+import DreamForm from '@/components/DreamForm';
+
+export default function TabOneScreen() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Viens chez moi, mes toilettes sont bouchées</Text>
+      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
+      <EditScreenInfo path="app/(tabs)/index.tsx" />
+      <DreamForm/>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    overflowY : "auto",
+    overflowX : "none"
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+  separator: {
+    marginVertical: 30,
+    height: 1,
+    width: '80%',
+  },
+});
---
+++ my-app/components/SearchBar.tsx
@@ -0,0 +1,19 @@
+import SearchBar from 'react-native-search-bar';
+
+
+export default async function searchBar() {
+    <SearchBar
+    ref="searchBar"
+    placeholder="Search"
+    onChangeText={() => {
+        return null
+    }}
+    onSearchButtonPress={() => {
+        return null
+    }}
+    onCancelButtonPress={() => {
+        return null
+    }}
+    />
+    
+}
